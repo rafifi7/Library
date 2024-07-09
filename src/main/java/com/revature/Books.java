e4fa276335350a161c852649a306d74ab0c8d3a4
rafifi7 <rafeeadnan7@gmail.com>
2024-07-08 22:51:54 -0400

    start of project0

---
+++ src/main/java/com/revature/Books.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+public class Books {
+    String title;
+    String author;
+    String genre;
+    int copiesAvailable;
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
